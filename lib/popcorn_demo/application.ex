ca28d6e3c48a0b0f39f19e7cb943075b0ea56a3f
FRICK-ELDY <ncssano0x0000@gmail.com>
2025-11-20 10:25:20 +0900

    refactoring apps

--- lib/popcorn_demo/application.ex
+++ lib/popcorn_demo/application.ex
@@ -5,7 +5,6 @@ defmodule PopcornDemo.Application do
   def start(_type, _args) do
     children = [
 			PopcornDemo.Worker,
-			# 先に Ticker を起動し、Popcorn.Wasm.register/1 を完了してから他の Task を流す
 			PopcornDemo.Ticker,
 			PopcornDemo.Hello,
 			PopcornDemo.Parallel
