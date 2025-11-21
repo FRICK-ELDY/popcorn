64f45a9f4db6ddae9220ce0ab4a6692232c552d0
FRICK-ELDY <ncssano0x0000@gmail.com>
2025-11-21 22:07:30 +0900

    fix

--- lib/popcorn_demo/super_crash.ex
+++ lib/popcorn_demo/super_crash.ex
@@ -2,50 +2,39 @@ defmodule PopcornDemo.SuperCrash do
 	@moduledoc false
 
 	@max_restarts 5
-	@interval_ms 100
 
 	def run(max \\ @max_restarts, interval_ms \\ @interval_ms) when max >= 0 and interval_ms > 0 do
 		start_ms = System.monotonic_time(:millisecond)
 		IO.puts("[sup] supervisor starting (max=#{max})")
-		{:ok, mgr} = GenServer.start_link(__MODULE__.Manager, %{max: max, start_ms: start_ms})
-		GenServer.cast(mgr, :start)
+		setup_ets(max, start_ms)
+
+		children = [
+			%{
+				id: __MODULE__.Crashy,
+				start: {__MODULE__.Crashy, :start_link, [%{}]},
+				restart: :permanent,
+				shutdown: 2_000,
+				type: :worker
+			}
+		]
+
+		{:ok, _sup} =
+			Supervisor.start_link(children,
+				strategy: :one_for_one,
+				max_restarts: max + 2,
+				max_seconds: 2
+			)
 		:ok
 	end
 
-	defmodule Manager do
-		use GenServer
-
-		def start_link(args), do: GenServer.start_link(__MODULE__, args)
-
-		@impl true
-		def init(%{max: max, start_ms: start_ms}) do
-			{:ok, %{max: max, start_ms: start_ms, attempt: 0, child: nil, ref: nil}}
+	defp setup_ets(max, start_ms) do
+		case :ets.whereis(:sup_demo) do
+			:undefined -> :ets.new(:sup_demo, [:named_table, :public])
+			_ -> :ok
 		end
-
-		@impl true
-		def handle_cast(:start, %{attempt: attempt, max: max} = state) do
-			next = attempt + 1
-			if next <= max do
-				{:ok, pid} =
-					PopcornDemo.SuperCrash.Crashy.start_link(%{attempt: next, max: max, start_ms: state.start_ms})
-				ref = Process.monitor(pid)
-				{:noreply, %{state | attempt: next, child: pid, ref: ref}}
-			else
-				ms = System.monotonic_time(:millisecond) - state.start_ms
-				IO.puts("[sup] recovered after #{max} restarts")
-				IO.puts("[sup] done in #{ms} ms")
-				{:noreply, state}
-			end
-		end
-
-		@impl true
-		def handle_info({:DOWN, ref, :process, pid, reason}, %{ref: ref} = state) do
-			GenServer.cast(self(), :start)
-			{:noreply, %{state | child: nil, ref: nil}}
-		end
-
-		@impl true
-		def handle_info(_msg, state), do: {:noreply, state}
+		:ets.insert(:sup_demo, {:restarts, 0})
+		:ets.insert(:sup_demo, {:max, max})
+		:ets.insert(:sup_demo, {:start_ms, start_ms})
 	end
 
 	defmodule Crashy do
@@ -54,13 +43,30 @@ defmodule PopcornDemo.SuperCrash do
 		def start_link(args), do: GenServer.start_link(__MODULE__, args)
 
 		@impl true
-		def init(%{attempt: attempt, max: max, start_ms: start_ms}) do
+		def init(_args) do
+			attempt = :ets.update_counter(:sup_demo, :restarts, {2, 1}, {:restarts, 0})
+			start_ms =
+				case :ets.lookup(:sup_demo, :start_ms) do
+					[{:start_ms, v}] -> v
+					_ -> System.monotonic_time(:millisecond)
+				end
+			max =
+				case :ets.lookup(:sup_demo, :max) do
+					[{:max, v}] -> v
+					_ -> 0
+				end
 			IO.puts("[sup] worker started")
 			IO.puts("[sup] crashing (#{attempt}/#{max})")
-			# Supervisorを使わないので自力で即時終了を行う
-			send(self(), :shutdown)
 			state = %{attempt: attempt, max: max, start_ms: start_ms}
-			{:ok, state}
+			if attempt <= max do
+				send(self(), :shutdown)
+				{:ok, state}
+			else
+				ms = System.monotonic_time(:millisecond) - start_ms
+				IO.puts("[sup] recovered after #{max} restarts")
+				IO.puts("[sup] done in #{ms} ms")
+				{:ok, state}
+			end
 		end
 
 		@impl true
